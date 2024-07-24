2e6c7e8db5b3350bb5dd77d56fa4024acdd5da1e
Danilo Just <92367836+jus-dan@users.noreply.github.com>
2024-07-24 11:10:09 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,23 @@
+basic.showIcon(IconNames.No)
+OLED.init(128, 64)
+IoTCube.LoRa_Join(
+eBool.enable,
+eBool.enable,
+10,
+8
+)
+OLED.writeStringNewLine("Hello World!")
+basic.showIcon(IconNames.Yes)
+loops.everyInterval(5000, function () {
+    if (IoTCube.getStatus(eSTATUS_MASK.JOINED)) {
+        OLED.clear()
+        OLED.writeStringNewLine("Lichtstärke: " + input.lightLevel())
+        basic.showIcon(IconNames.SmallHeart)
+        IoTCube.addIlluminance(input.lightLevel(), 1)
+        IoTCube.addAnalogInput(input.soundLevel(), 2)
+        IoTCube.SendBuffer(IoTCube.getCayenne())
+        basic.showIcon(IconNames.Heart)
+    } else {
+        basic.showIcon(IconNames.Sad)
+    }
+})
